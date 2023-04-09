b7d90fe5a7734a11b81732258bc2da7ddc44026f
Omar Assouma <assouma@sabanciuniv.edu>
2023-04-09 17:09:29 +0300

    created about us section

---
+++ src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../sass/AboutUs.scss'
+
+export const AboutUs = () => {
+  return (
+    <div className="aboutUs">
+      <div className="information_box">
+        <span className='title'>Who are we?</span>
+        <span className='information'>We are a group of highly skilled computer science and industrial engineers who share a passion for entrepreneurship and innovation. Our team is dedicated to building the ultimate education hub for aspiring entrepreneurs and innovators, providing them with the knowledge, resources, and support they need to succeed</span>
+      </div>
+      
+    </div>
+  )
+}
