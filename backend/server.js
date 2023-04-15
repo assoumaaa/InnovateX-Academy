e252a84b7ef96ebf390b146d78d7f59c552f9df3
Jeren <jerena@sabanciuniv.edu>
2023-04-15 18:12:54 +0300

    added logout

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const firebase = require('firebase/app');
 require('firebase/auth');
 
-// Firebase configuration 
 const firebaseConfig = {
     apiKey: "AIzaSyAnVwd09ZECF1DVjn0y34Jiqxu9_obn7to",
     authDomain: "academy-x-87af5.firebaseapp.com",
@@ -18,48 +17,52 @@ firebase.initializeApp(firebaseConfig);
 const app = express();
 const port = 3000; 
 
-// Middlewares
 app.use(express.json());
 
-// Routes
-
-
 app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        // Sign in with email and password using Firebase Authentication
-        const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
-        // Retrieve user data from userCredential.user
-        const user = userCredential.user;
-        // Send appropriate response
-        res.send(`Logged in successfully! User ID: ${user.uid}`);
-      } catch (error) {
-        // Handle error and send appropriate response
-        res.status(500).send(`Login failed: ${error.message}`);
-    }
+  try {
+      const { email, password } = req.body;
+      const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const user = userCredential.user;
+      res.send(`Logged in successfully! User ID: ${user.uid}`);
+    } catch (error) {
+      res.status(500).send(`Login failed: ${error.message}`);
+  }
 });
 
-// Route for sign up
 app.post('/signup', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        // Sign up with email and password using Firebase Authentication
-        const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-        // Retrieve user data from userCredential.user
-        const user = userCredential.user;
-        // Send appropriate response
-        res.send(`Signed up successfully! User ID: ${user.uid}`);
-      } catch (error) {
-        // Handle error and send appropriate response
-        res.status(500).send(`Signup failed: ${error.message}`);
-    }
+  try {
+      const { email, password } = req.body;
+      const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      const user = userCredential.user;
+      res.send(`Signed up successfully! User ID: ${user.uid}`);
+    } catch (error) {
+      res.status(500).send(`Signup failed: ${error.message}`);
+  }
 });
 
-// Route for main page
+// Logout route
+app.post('/logout', async (req, res) => {
+  try {
+    await firebase.auth().signOut();
+    res.send('Logged out successfully!');
+  } catch (error) {
+    res.status(500).send(`Logout failed: ${error.message}`);
+  }
+});
+
+
+app.post('/forgotpassword', async (req, res) => {
+  try {
+    const { email } = req.body;
+    await firebase.auth().sendPasswordResetEmail(email);
+    res.send(`Password reset email sent to ${email}`);
+  } catch (error) {
+    res.status(500).send(`Failed to send password reset email: ${error.message}`);
+  }
+});
+
 app.get('/', (req, res) => {
-  // Handle main page logic here
-  // Render main page HTML, retrieve data from database, etc.
-  // Send appropriate response
   res.send('Main page route');
 });
 
