c9947b6132b826476f4b2d11baf9b06ee7168188
Omar Assouma <assouma@sabanciuniv.edu>
2023-04-17 15:03:01 +0300

    fixed structure

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -8,8 +8,9 @@ export const Modules = () => {
   const modules = ["Module 0", "Module 1", "Module 2", "Module 3", "Module 4", "Module 5", "Module 6", "Module 7", "Module 8", "Module 9", "Module 10", "Module 11", "Module 12", "Module 13", "Module 14"];
   const modules_names = ["Design Thinking", "Six Thinking Hats", "Selection", "Persona", "Jobs To Be Done", "Value Proposition", "POV", "Ideation", "Prototype", "Module 9", "Module 10", "Module 11", "Module 12", "Module 13", "Module 14"];
   const hrefs = ['/modules/m0', '/modules/m1', '/modules/m2', '/modules/m3', '/modules/m4', '/modules/m5', '/modules/m6', '/modules/m7', '/modules/m8', '/modules/m9', '/modules/m10', '/modules/m11', '/modules/m12', '/modules/m13', '/modules/m14']
+
   
-  
+
 
   const navigate = useNavigate();
   const handleBack = () => {
@@ -18,8 +19,9 @@ export const Modules = () => {
 
   useEffect(() => {
     const currentPath = window.location.pathname;
-    const selectedIndex = hrefs.findIndex(href => href === currentPath);
+    const selectedIndex = hrefs.findIndex(href => currentPath.startsWith(href));
     setSelectedIndex(selectedIndex);
+    console.log('inside useEffect' + hrefs.findIndex(href => href === currentPath.startsWith(href)));
   }, [hrefs]);
 
   return (
@@ -28,15 +30,14 @@ export const Modules = () => {
 
         <div className="logo">
           <img src='../../images/logo.png' alt='logo' onClick={handleBack} />
-          <span>InnovateX Academy</span>
         </div>
 
         <div className="modules">
           {modules.map((module, index) => (
             <a key={index} href={hrefs[index]}>
               <div className={`${index === selectedIndex ? 'eachModule selected' : 'eachModule'}`} onClick={() => setSelectedIndex(index)}>
-                <h1>{module} </h1>
-                <span className='modules_names'> {modules_names[index]}</span>
+                <h1 className='modules_names'> {modules_names[index]}</h1>
+                <span>{module} </span>
               </div>
             </a>
           ))}
---
+++ src/modules/M0/M0.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Modules } from '../../components/Modules';
+import { Start } from './Start';
+import { Definition } from './Definition';
+import '../../sass/Modules/M0/M0.scss'
+
+
+
+
+export const M0 = () => {
+
+    const [def, SetDef] = useState(false)
+    const handleDef = () => {
+        SetDef(true)
+    }
+
+    return (
+        <div className="m0">
+            <Modules />
+            {!def ? (
+                <Start SetDef={SetDef} />
+            ) : (
+                <Definition SetDef={SetDef} />
+            )}
+        </div>
+
+    )
+}
+
+//<img src='../images/m0.png'/>
