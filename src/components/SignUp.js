9380282274569455c6f9362e4412e7a14da8492c
Omar Assouma <assouma@sabanciuniv.edu>
2023-04-20 16:05:42 +0300

    fixed login/signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react'
 import '../sass/Login.scss'
 import { useNavigate } from 'react-router-dom';
 import { BsArrowLeft } from "react-icons/bs";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 
 
 export const SignUp = ({ setSignUp }) => {
 
-
+    const auth = getAuth();
     const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleBack = () => {
         navigate('/')
+    }
+
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {  
+                console.log("I JUST REGISTERED A USER!!!!!")
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode + errorMessage)                
+            });
     }
 
     const handleLogin = () => {
@@ -29,18 +49,18 @@ export const SignUp = ({ setSignUp }) => {
                 <form>
                     <div className="form-group">
                         <label htmlFor="email">Full Name</label>
-                        <input type="text" className="form-control" id="email" placeholder="Full Name" />
+                        <input type="text" className="form-control"  placeholder="Full Name" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email address</label>
-                        <input type="email" className="form-control" id="email" placeholder="Enter email" />
+                        <input type="email" className="form-control" id="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="Password" />
+                        <input type="password" className="form-control" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="button">
-                        <button type="submit" className="btn btn-primary">Sign Up</button>
+                        <button type="submit" className="btn btn-primary" onClick={handleSignUp}>Sign Up</button>
                         <span>Already have an account? <a onClick={handleLogin}>Login</a></span>
                     </div>
                 </form>
