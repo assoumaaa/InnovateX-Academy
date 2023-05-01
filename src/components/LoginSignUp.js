df4212956da7f7996cc0943386487069945f91ac
Omar Assouma <assouma@sabanciuniv.edu>
2023-05-01 22:07:59 +0300

    added login-image

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -15,7 +15,7 @@ export const LoginSignUp = () => {
         <div className="logInSignUp">
             <div className="left">
                 <span className='slogan'>Innovate. Empower. Succeed.</span>
-                <img src='../../images/login.jpg' alt='innovate' />
+                <img src='../../images/login.jpeg' alt='innovate' />
             </div>
 
             {signUp ? (
