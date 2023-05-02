9617ad655368aea4c52ab25638f4b6bd5aa03879
Omar Assouma <assouma@sabanciuniv.edu>
2023-05-02 17:23:41 +0300

    latest

--- src/modules/M0/Start.js
+++ src/modules/M0/Start.js
@@ -3,10 +3,6 @@ import '../../sass/Modules/M0/Start.scss'
 
 export const Start = ({ SetDef }) => {
 
-    const handleDef = () => {
-        SetDef(true)
-    }
-
     return (
         <div className="m0">
             <div className="wrapper">
@@ -14,12 +10,13 @@ export const Start = ({ SetDef }) => {
                     <div className="left-wrapper">
                         <h1>Design Thinking?</h1>
                         <span>The main objective of this module is to define the problem that the entrepreneur will tackle in this program.</span>
-                        <button onClick={handleDef}> Start </button>
+                        <button onClick={() => SetDef(true)}> Start </button>
                     </div>
                 </div>
 
-            <div className="right">
-                <img src='../../images/m0.jpeg' alt='m0' />
+                <div className="right">
+                    <img src='../../images/m0.jpeg' alt='m0' />
+                </div>
             </div>
         </div>
     )
